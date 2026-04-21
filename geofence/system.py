d71a64bbe69338e6cb4468e0c11df062c5fe99d9
Yoali Cid <yoalicid12@gmail.com>
2026-04-20 17:57:50 -0700

    system class with properties

---
+++ geofence/system.py
@@ -0,0 +1,35 @@
+from .locations import Location
+from .devices import Device
+from .zones import Zone
+from .alerts import Alert
+
+class System:
+    def __init__(self, system:str):
+        self.system = system 
+        self.__zone_registry:list[Zone] = []
+        self.__device_registry:list[Device]=[]
+        self.__location_registry:list[Location]=[]
+        self.__alerts:list[Alert]=[]
+
+    @property
+    def zone_registry(self):
+        return self.__zone_registry
+    
+    @property
+    def device_registry(self):
+        return self.__device_registry
+    
+    @property 
+    def location_registry(self):
+        return self.__location_registry
+    
+    @property 
+    def alerts(self):
+        return self.__alerts
+    
+    def update_device__location(self,location: Location):
+        ...
+
+    def alert_logs(self ):
+        ...
+
