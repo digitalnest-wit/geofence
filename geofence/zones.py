6a7e9620ab01ceae423fcf6d4ad10111022138dd
Jesus Ortiz <joortizjesus@gmail.com>
2026-04-22 17:39:59 -0700

    Implemented contains in zones.py

--- geofence/zones.py
+++ geofence/zones.py
@@ -1,5 +1,5 @@
 from .locations import Location
-
+from math import sqrt
 
 class Zone:
     def __init__(self, name:str, location:Location, radius:float):
@@ -7,5 +7,9 @@ class Zone:
         self.location = location 
         self.radius = radius 
 
-        def contains(self, location:Location)-> bool:
-            ...
+    def contains(self, location:Location)-> bool:
+        distance = sqrt(
+            (self.location.x - location.x) ** 2 + 
+            (self.location.y - location.y)
+        )     
+        return distance <= self.radius 
